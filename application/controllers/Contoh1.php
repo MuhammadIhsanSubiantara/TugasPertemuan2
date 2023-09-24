e62b8d53c8346ef6da33d886deb57baeff465560
MuhammadIhsanSubiantara <ihsansubiantara3@gmail.com>
2023-09-24 21:18:31 +0700

    Tugas Pertemuan 2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+	public function index()
+	{
+	echo "<h1>Perkenalkan</h1>";
+          echo"Nama saya Muhammad Ihsan Subiantara
+          Saya tingga di daerah Ciomas
+          hobi saya adalah zikir
+          ";
+	}
+}
